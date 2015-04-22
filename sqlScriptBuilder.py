df22609531a171032717505006c59d05a64ccf16
dimitri-schmidt <dimitri.schmidt@student.hpi.uni-potsdam.de>
2015-04-22 11:24:26 +0200

    initial port from old repo

---
+++ sqlScriptBuilder.py
@@ -0,0 +1,319 @@
+from lxml import html
+import requests
+from collections import OrderedDict
+from operator import itemgetter
+import time
+import codecs
+import unicodedata
+import os
+import json
+import uuid
+
+class sqlScriptBuilder:
+
+	MAX_PROPERTY_NUMBER = 2000
+	MAX_SQL_LINE_NUMBER = 455
+	SQL_FILE_NAME = "fill_wdqa_constraints_from_templates_with_json_blob.sql"
+
+	SQL_SCRIPT_HEAD = "INSERT INTO wdqa_constraints (constraint_guid, pid, constraint_type_qid, constraint_parameters) VALUES\n"
+
+	parameters = {}
+
+	def find_next_seperator(self, constraint_parameters, equal_sign):
+		next_equal_sign = constraint_parameters.find('=', equal_sign + 1)
+		if next_equal_sign == -1:
+			next_seperator = len(constraint_parameters)
+		else:
+			next_seperator = constraint_parameters.rfind('|', equal_sign, next_equal_sign)
+		if next_seperator == -1:
+			next_seperator = len(constraint_parameters)
+		else:
+			next_seperator = next_seperator + 1
+		return next_seperator
+
+	def to_comma_seperated_string(self, values):
+		return values.replace("{", "").replace("}", "").replace("|", "").replace(" ", "").replace("[", "").replace("]", "").strip()
+
+	def add_property(self, values):
+		self.parameters['property'] = values.strip()
+
+	def add_classes(self, values):
+		self.parameters['class'] = self.to_comma_seperated_string(values)
+
+	def add_exceptions(self, values):
+		self.parameters['known_exception'] = self.to_comma_seperated_string(values).replace(";", ",")
+
+	def add_group_by(self, values):
+		self.parameters['group_by'] = values.strip()
+
+	def add_items(self, values):
+		itemString = ""
+		snakString = ""
+		for element in self.to_comma_seperated_string(values).split(","):
+			if element.startswith("Q"):
+				itemString = itemString + element + ","
+			elif element.lower() == "somevalue" or element.lower() == "novalue":
+				snakString = snakString + element + ","
+		if itemString != "":
+			self.parameters['item'] = itemString.rstrip(",")
+		if snakString != "":
+			self.parameters['snak'] = snakString.rstrip(",")
+
+	def add_list(self, values, constraint_name):
+		if constraint_name == "Qualifiers" or constraint_name == "Mandatory qualifiers":
+			self.parameters['property'] = self.to_comma_seperated_string(values)
+		else:
+			self.list_parameter = self.to_comma_seperated_string(values)
+
+	def add_status(self, values):
+		self.parameters['constraint_status'] = 'mandatory'
+
+	def add_max(self, values):
+		#if "0000" in values or "." in values:
+		#	self.parameters['maximum date'] = values
+		#else:
+		self.parameters['maximum_quantity'] = values.strip()
+
+	def add_min(self, values):
+		#if "0000" in values or "." in values:
+		#	self.parameters['minimum date'] = values
+		#else:
+		self.parameters['minimum_quantity'] = values.strip()
+
+	def add_namespace(self, values):
+		self.parameters['namespace'] = values.strip()
+
+	def add_pattern(self, values):
+		self.parameters['pattern'] = values.replace("\\","\\\\").strip()
+
+	def add_relation(self, values):
+		self.parameters['relation'] = values.strip()	
+
+	def write_one_line(self, property_number, constraint_name):
+		self.write_first_three_columns(property_number, constraint_name)
+		self.write_blob()
+		self.reset_parameter()
+
+	def write_multiple_lines(self, property_number, constraint_name):
+		for line in self.list_parameter.split(';'):
+			self.write_first_three_columns(property_number, constraint_name)
+			self.split_list_parameter(line)
+			self.write_blob()	
+			self.parameters.pop('item', None)
+		self.reset_parameter
+
+	def write_line_in_sql_file(self, property_number, constraint_name):
+		if self.list_parameter != 'NULL':
+			self.write_multiple_lines(property_number, constraint_name)
+		else:
+			self.write_one_line(property_number, constraint_name)
+
+	def split_list_parameter(self, line):
+		if ':' in line:
+			self.parameters['item'] = line[line.index(':')+1:]
+			self.parameters['property'] = line[:line.index(':')]
+		else:
+			self.parameters['property'] = line
+
+
+	def write_first_three_columns(self, property_number, constraint_name):
+		self.outputString += ( '(' + '"' + str(uuid.uuid4()) + '", ' + format(property_number) + ', \"' + constraint_name.strip() + '\", ' )
+
+	def write_blob(self):
+		json_blob_string = json.dumps(json.dumps(self.parameters)).replace("&lt;nowiki>","").replace("&lt;/nowiki>","").replace("&amp;lt;nowiki&amp;lt;","").replace("&amp;lt;/nowiki&amp;gt;","").replace("<nowiki>","").replace("</nowiki>","")
+		self.outputString += json_blob_string
+		self.outputString += ("),\n")
+
+	def reset_parameter(self):
+		self.parameters = {}
+		self.list_parameter = 'NULL'
+
+	def writeOutputStringToFile(self):
+		print("writing into " + self.SQL_FILE_NAME)
+		with codecs.open(self.SQL_FILE_NAME, "a", "utf-8") as sql_file:
+			self.outputString = self.outputString.rstrip(",\n") + ";\n\n"
+			sql_file.write(self.outputString)
+
+		self.writtenLinesInInsertStatement = 0
+		self.outputString = self.SQL_SCRIPT_HEAD
+
+	def get_constraint_part(self, property_talk_page):
+		start = property_talk_page.find("{{Constraint:")
+		 	
+		end = property_talk_page.find("==")
+		if( end != -1):
+			property_talk_page = property_talk_page[start:end]
+		else:
+			property_talk_page = property_talk_page[start:]
+
+		#delete <!-- --> comments from site
+		open_index = property_talk_page.find("&lt;!--")
+		while (open_index) != -1:
+			close_index = property_talk_page.find("-->", open_index)
+			if(close_index == -1):
+				break
+				
+			property_talk_page = property_talk_page[:open_index] + property_talk_page[close_index+3:]
+			
+			open_index = property_talk_page.find("&lt;!--")	
+		return property_talk_page
+
+
+	def progress_print(self, number, maxNumber):
+		if number % 10 == 0:
+			print(str(number) + "/" + str(maxNumber))
+
+
+	def property_exists(self, propertyTalkPage):
+		return not (propertyTalkPage.find("Creating Property talk") != -1 or propertyTalkPage == "")
+
+
+	def get_constraint_end_index(self, constraintPart):
+		count = 2
+		for i, c in enumerate(constraintPart):
+			if c == '{':
+				count += 1
+			elif c == '}':
+				count -= 1
+			if count == 0:
+				return (i - 1)
+				# end_index = i-1
+				# break
+
+
+	# only purpose: Build SQL-Statement to fill table with constraints
+	# fetches constraints from property talk pages
+	# nonetheless: use table layout that will suit the new way of storing 
+	# constraints as statements on properties
+
+	def run(self):
+		# delete old file
+		if os.path.exists(self.SQL_FILE_NAME):
+			os.remove(self.SQL_FILE_NAME)
+
+		self.writtenLinesInInsertStatement = 0
+		self.outputString = self.SQL_SCRIPT_HEAD
+
+		#this is how every constraint template begins
+		search_string = "{{Constraint:"
+
+		for property_number in range(1, self.MAX_PROPERTY_NUMBER+1):
+
+			if (self.writtenLinesInInsertStatement > self.MAX_SQL_LINE_NUMBER):
+				self.writeOutputStringToFile()
+
+
+			#self.outputString += (30*"=" + "Property " + format(property_number) + 30*"=")
+			self.progress_print(property_number, self.MAX_PROPERTY_NUMBER)
+
+			property_talk_page = requests.get("http://www.wikidata.org/w/index.php?title=Property_talk:P" + str(property_number) + "&action=edit").text
+			
+			if self.property_exists(property_talk_page):
+
+				#delete <nowiki> </nowiki> tags from property_talk_page 
+				constraintPart = self.get_constraint_part(property_talk_page)
+
+				# indices for the first and last character belonging to a respective constraint
+				start_index = end_index = None
+				
+				# find beginning of constraint
+				start_index = constraintPart.find(search_string)
+
+				#as long as there are more constraints, set new start index and cut off everything before it
+				while start_index != -1:
+					start_index += len(search_string)
+					constraintPart = constraintPart[start_index:]
+
+					#match brackets to find end of constraint
+					end_index = self.get_constraint_end_index(constraintPart)
+					# count = 2
+					# for i, c in enumerate(constraintPart):
+					# 	if c == '{':
+					# 		count += 1
+					# 	elif c == '}':
+					# 		count -= 1
+					# 	if count == 0:
+					# 		end_index = i-1
+					# 		break
+					
+					#extract constraint
+					constraint_string = constraintPart[:end_index]
+
+					
+					constraint_name = None
+					constraint_parameters = None
+					self.list_parameter = 'NULL'
+
+					delimiter_index = constraint_string.find('|')
+
+					if delimiter_index == -1:
+						constraint_name = constraint_string
+					else:			
+						constraint_name = constraint_string[:delimiter_index]
+						constraint_parameters = constraint_string[delimiter_index+1:]
+
+						while constraint_parameters != None and constraint_parameters.find('=') != -1:
+							equal_sign = constraint_parameters.find('=')
+							next_seperator = self.find_next_seperator(constraint_parameters, equal_sign)
+							a = 0 if next_seperator == -1 else -1
+							parameter_name = constraint_parameters[:equal_sign].strip()
+							parameter_value = constraint_parameters[equal_sign+1:next_seperator+a]
+							
+							if parameter_name == 'base_property':
+								self.add_property(parameter_value)
+							elif parameter_name == 'class' or parameter_name == 'classes':
+								self.add_classes(parameter_value)
+							elif parameter_name == 'exceptions':
+								self.add_exceptions(parameter_value)
+							elif parameter_name == 'group by' or parameter_name == 'group property':
+								self.add_group_by(parameter_value)
+							elif parameter_name == 'item' or parameter_name == 'items':
+								self.add_items(parameter_value)
+							elif parameter_name == 'list':
+								self.add_list(parameter_value, constraint_name)
+							elif parameter_name == 'mandatory':
+								self.add_status(parameter_value)
+							elif parameter_name == 'max':
+								self.add_max(parameter_value)
+							elif parameter_name == 'min':
+								self.add_min(parameter_value)
+							elif parameter_name == 'namespace':
+								self.add_namespace(parameter_value)
+							elif parameter_name == 'pattern':
+								self.add_pattern(parameter_value)
+							elif parameter_name == 'property':
+								self.add_property(parameter_value)
+							elif parameter_name == 'relation':
+								self.add_relation(parameter_value)
+							elif parameter_name == 'value' or parameter_name == 'values':
+								self.add_items(parameter_value)
+							elif parameter_name == 'required' and parameter_value == 'true':
+								constraint_name = 'Mandatory qualifiers'
+							
+
+
+							constraint_parameters = constraint_parameters[next_seperator:]
+					
+				
+					self.write_line_in_sql_file(property_number, constraint_name)
+					self.writtenLinesInInsertStatement += 1
+
+					#prepare search for new constraint
+					constraintPart = constraintPart[end_index:]
+					start_index = constraintPart.find(search_string)
+					authority_pos = constraintPart.find('{{Authority control properties}}') 
+					normalization_pos = constraintPart.find('== parameter value normalization ==')	
+					end_of_constraint_section = authority_pos if normalization_pos < authority_pos else normalization_pos
+					if start_index > end_of_constraint_section and end_of_constraint_section != -1:
+						break
+
+
+		if self.outputString != self.SQL_SCRIPT_HEAD:
+			self.writeOutputStringToFile()
+
+
+def main():
+	builder = sqlScriptBuilder()
+	builder.run()
+
+if __name__ == "__main__": main()
---
+++ test_sqlScriptBuilder.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+
+from sqlScriptBuilder import sqlScriptBuilder
+
+class TestSqlScriptBuilder():
+
+    def setup_method(self, method):
+        #  setup_method is invoked for every test method of a class
+        self.builder = sqlScriptBuilder()
+
+
+    def test_to_comma_seperated_string_standard(self):
+        test_string = "{{Q|6581072}}, {{Q|43445}}, {{Q|1052281}}"
+        expected_result = "Q6581072,Q43445,Q1052281"
+        result = self.builder.to_comma_seperated_string(test_string)
+        assert result == expected_result
+
+
+    def test_to_comma_seperated_string_empty(self):
+        test_string = ""
+        expected_result = ""
+        result = self.builder.to_comma_seperated_string(test_string)
+        assert result == expected_result       
+
+
+    def test_to_comma_seperated_string_unicode(self):
+        test_string = "¡“¶¢[]|{}≠¿'≤¥ç √~∫µ…–∞≈å  æœ∂‚ƒ@© πª•ºπ«∑€ ®†Ω¨⁄øπ•   ±‘æœ@∆‚å≈~–"
+        expected_result = "¡“¶¢≠¿'≤¥ç√~∫µ…–∞≈åæœ∂‚ƒ@©πª•ºπ«∑€®†Ω¨⁄øπ•±‘æœ@∆‚å≈~–"
+        result = self.builder.to_comma_seperated_string(test_string)
+        assert result == expected_result
+
+
+    def test_add_property_standard(self):
+        test_value = "P1337"
+        expected_result = "P1337"
+        self.builder.add_property(test_value)
+        assert self.builder.parameters['property'] == expected_result
+
+
+    def test_add_property_whitepace(self):
+        test_value = " P7331 "
+        expected_result = "P7331"
+        self.builder.add_property(test_value)
+        assert self.builder.parameters['property'] == expected_result
+
+
+    def test_add_property_empty(self):
+        test_value = ""
+        expected_result = ""
+        self.builder.add_property(test_value)
+        assert self.builder.parameters['property'] == expected_result
+
+
+    def test_add_property_multiple(self):
+        expected_result = ""
+        assert self.builder.parameters['property'] == expected_result
+
+        test_value = "P2992"
+        expected_result = "P2992"
+        self.builder.add_property(test_value)
+        assert self.builder.parameters['property'] == expected_result
+
+        test_value = "P1234567890"
+        expected_result = "P1234567890"
+        self.builder.add_property(test_value)
+        assert self.builder.parameters['property'] == expected_result
+
+
+    def test_add_classes_standard(self, monkeypatch):
+        def mockreturn(path):
+            return "Q5,Q95074"
+        monkeypatch.setattr(self.builder, 'to_comma_seperated_string', mockreturn)
+        test_value = "Q5,Q95074"
+        expected_result = "Q5,Q95074"
+        self.builder.add_classes(test_value)
+        assert self.builder.parameters['class'] == expected_result
+
+
+    def test_add_classes_empty(self, monkeypatch):
+        def mockreturn(path):
+            return ""
+        monkeypatch.setattr(self.builder, 'to_comma_seperated_string', mockreturn)
+        test_value = ""
+        expected_result = ""
+        self.builder.add_classes(test_value)
+        assert self.builder.parameters['class'] == expected_result
+
+
+    def test_add_classes_multiple(self, monkeypatch):
+        def first(path):
+            return "first"
+        def second(path):
+            return "second"
+        monkeypatch.setattr(self.builder, 'to_comma_seperated_string', first)
+        test_value = "first"
+        expected_result = "first"
+        self.builder.add_classes(test_value)
+        assert self.builder.parameters['class'] == expected_result
+
+        monkeypatch.setattr(self.builder, 'to_comma_seperated_string', second)
+        test_value = "second"
+        expected_result = "second"
+        self.builder.add_classes(test_value)
+        assert self.builder.parameters['class'] == expected_result
+
+
+    def test_add_exceptions_standard(self, monkeypatch):
+        def mockreturn(path):
+            return "Q18646002;Q18646076"
+        monkeypatch.setattr(self.builder, 'to_comma_seperated_string', mockreturn)
+        test_value = " {{Q|18646002}}; {{Q|18646076}} "
+        expected_result = "Q18646002,Q18646076"
+        self.builder.add_exceptions(test_value)
+        assert self.builder.parameters['known_exception'] == expected_result
+
+
+    def test_progress_print_standard(self, capsys):
+        self.builder.progress_print(0, 2000)
+        out, err = capsys.readouterr()
+        expectedOut = '0/2000\n'
+        assert out == expectedOut
+
+        self.builder.progress_print(1, 2000)
+        self.builder.progress_print(2, 2000)
+        self.builder.progress_print(3, 2000)
+        self.builder.progress_print(4, 2000)
+        self.builder.progress_print(5, 2000)
+        self.builder.progress_print(6, 2000)
+        self.builder.progress_print(7, 2000)
+        self.builder.progress_print(8, 2000)
+        self.builder.progress_print(9, 2000)
+        out, err = capsys.readouterr()
+        expectedOut = ''
+        assert out == expectedOut
+
+        self.builder.progress_print(10, 2000)
+        out, err = capsys.readouterr()
+        expectedOut = '10/2000\n'
+        assert out == expectedOut
+
+        self.builder.progress_print(11, 2000)
+        self.builder.progress_print(12, 2000)
+        self.builder.progress_print(13, 2000)
+        self.builder.progress_print(14, 2000)
+        self.builder.progress_print(15, 2000)
+        self.builder.progress_print(16, 2000)
+        self.builder.progress_print(17, 2000)
+        self.builder.progress_print(18, 2000)
+        self.builder.progress_print(19, 2000)
+        out, err = capsys.readouterr()
+        expectedOut = ''
+        assert out == expectedOut
+
+        self.builder.progress_print(20, 2000)
+        out, err = capsys.readouterr()
+        expectedOut = '20/2000\n'
+        assert out == expectedOut
+
+
+    def test_progress_print_negative(self, capsys):
+        self.builder.progress_print(-1, 1234)
+        out, err = capsys.readouterr()
+        expectedOut = ''
+        assert out == expectedOut
+
+        self.builder.progress_print(-10, 1234)
+        out, err = capsys.readouterr()
+        expectedOut = '-10/1234\n'
+        assert out == expectedOut
+
+
+    def test_property_exists_false(self):
+        test_html = '<!DOCTYPE html><html lang="en" dir="ltr" class="client-nojs"><head><meta charset="UTF-8" /><title>Creating Property talk:P2 - Wikidata</title><meta name="generator" content="MediaWiki 1.26wmf1" /><meta name="robots" content="noindex,nofollow" /><link rel="alternate" type="application/x-wiki" title="Edit" href="/w/index.php?title=Property_talk:P2&amp;action=edit" /><link rel="edit" title="Edit" href="/w/index.php?title=Property_talk:P2&amp;action=edit" /></head><body><p>Dis is ma fancy html page</p></body></html>'
+        expected_result = False
+        result = self.builder.property_exists(test_html)
+        assert result == expected_result
+
+
+    def test_property_exists_true(self):
+        test_html = '<!DOCTYPE html><html lang="en" dir="ltr" class="client-nojs"><head><meta charset="UTF-8" /><title>Editing Property talk:P9 - Wikidata</title><meta name="generator" content="MediaWiki 1.26wmf1" /><meta name="robots" content="noindex,nofollow" /><link rel="alternate" type="application/x-wiki" title="Edit" href="/w/index.php?title=Property_talk:P9&amp;action=edit" /><link rel="edit" title="Edit" href="/w/index.php?title=Property_talk:P9&amp;action=edit" /></head><body>Much content<br>such website<br>wow</body></html>'
+        expected_result = True
+        result = self.builder.property_exists(test_html)
+        assert result == expected_result
+
+
+    def test_property_exists_empty(self):
+        test_html = ''
+        expected_result = False
+        result = self.builder.property_exists(test_html)
+        assert result == expected_result
+
+
+    def test_get_constraint_end_index_standard(self):
+        test_constraintPart = 'Type|classes=Q1048835,Q56061|relation=instance}}\n{{Constraint:Value type|classes=Q5,Q95074|relation=instance|mandatory=true}}\n{{Constraint:Target required claim|property=P21}}\n{{Constraint:Target required claim|property=P39}}\n{{Constraint:Qualifiers|list={{P|580}}, {{P|582}} }}\n\n{{ExternalUse|\n* [[:cs:Šablona:Infobox - kraj]]\n* [[:la:Formula:Capsa civitatis Vicidata]]\n* [[:ru:Шаблон:НП]]\n* [[:ru:Шаблон:НП+]]\n* [[:ru:Шаблон:Данные о субъекте Российской Федерации]]\n* [[:ru:Шаблон:Регион Киргизии]]\n* [[:ru:Шаблон:НП/temp]]\n* [[:ru:Шаблон:Административная единица]], [[:ru:Шаблон:Субъект РФ]]\n* [[:ru:Шаблон:Аильный округ Киргизии]]\n}}\n\n'
+        expected_result = 46
+        result = self.builder.get_constraint_end_index(test_constraintPart)
+        assert result == expected_result
+
+
+    def test_get_constraint_end_index_second_standard(self):
+        test_constraintPart = 'Target required claim|property=P21|items={{Q|6581072}}, {{Q|43445}}, {{Q|1052281}}|mandatory=true}}\n{{Constraint:Type|classes=Q5,Q95074|relation=instance}}\n{{Constraint:Item|property=P21|mandatory=true}}\n{{Constraint:Item|property=P19}}\n{{Constraint:Item|property=P569}}\n{{Constraint:Qualifiers|list={{P|1039}}|mandatory=true}}\n\n{{Person properties}}\n[[Category: Reciprocal properties]]\n\n'
+        expected_result = 97
+        result = self.builder.get_constraint_end_index(test_constraintPart)
+        assert result == expected_result
+
+
+    def test_get_constraint_end_index_empty(self):
+        test_constraintPart = ''
+        expected_result = None
+        result = self.builder.get_constraint_end_index(test_constraintPart)
+        assert result == expected_result
+
+
+    def test_get_constraint_end_index_many_brackets(self):
+        test_constraintPart = '{{{}{{}{}}{}{}}}{}{{{{{}{{{}}{{}}}}}}}}}{}{{{}}{{}{}}}{{}}'
+        expected_result = 38
+        result = self.builder.get_constraint_end_index(test_constraintPart)
+        assert result == expected_result    
+
+
+
